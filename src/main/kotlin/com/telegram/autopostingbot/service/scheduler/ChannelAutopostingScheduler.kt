7d253707e7669fc88b51b076aca5633caa71b602
kalyashov <kalyashov@yandex.ru>
2021-10-07 21:47:09 +0300

    Add scheduler for post content to channel

--- src/main/kotlin/com/telegram/autopostingbot/service/scheduler/ChannelAutopostingScheduler.kt
+++ src/main/kotlin/com/telegram/autopostingbot/service/scheduler/ChannelAutopostingScheduler.kt
@@ -1,6 +1,31 @@
 package com.telegram.autopostingbot.service.scheduler
 
-interface ChannelAutopostingScheduler {
+import com.telegram.autopostingbot.AutopostingBot
+import com.telegram.autopostingbot.repository.content.ContentMetadataRepository
+import com.telegram.autopostingbot.repository.disk.YandexDiskClient
+import com.telegram.autopostingbot.service.ImagePostPrepareService
+import kotlinx.coroutines.runBlocking
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
-    fun postContent()
+@Service
+class ChannelAutopostingScheduler(
+    val contentMetadataRepository: ContentMetadataRepository,
+    val diskClient: YandexDiskClient,
+    val imagePostPrepareService: ImagePostPrepareService,
+    val bot: AutopostingBot
+) {
+
+    @Scheduled(cron = "\${autoposting-bot.scheduler.post-content-cron}")
+    fun postContent() = runBlocking {
+        val contentMetadata = contentMetadataRepository.getActual()
+        val img = diskClient.getImageByPath(contentMetadata.path)
+
+        val sendPhotoAction = imagePostPrepareService.prepareSendImageAction(img, contentMetadata.name)
+        bot.processAction(sendPhotoAction)
+
+        contentMetadata.postedAt = LocalDateTime.now()
+        contentMetadataRepository.save(contentMetadata)
+    }
 }
