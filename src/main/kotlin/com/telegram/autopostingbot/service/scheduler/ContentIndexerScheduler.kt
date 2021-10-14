f4de41cb0c9d5998ff3ede893cbd7cbd7f3971cc
kalyashov <kalyashov@yandex.ru>
2021-10-14 23:38:52 +0300

    Индексирование контента: добавил логирование

--- src/main/kotlin/com/telegram/autopostingbot/service/scheduler/ContentIndexerScheduler.kt
+++ src/main/kotlin/com/telegram/autopostingbot/service/scheduler/ContentIndexerScheduler.kt
@@ -6,6 +6,7 @@ import com.telegram.autopostingbot.repository.content.ContentMetadataRepository
 import com.telegram.autopostingbot.repository.disk.YandexDiskClient
 import com.telegram.autopostingbot.repository.scheduler.SchedulerInfoRepository
 import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
@@ -17,10 +18,13 @@ class ContentIndexerScheduler(val diskClient: YandexDiskClient,
 
     companion object {
         const val SCHEDULER_NAME = "content_indexer"
+        private val logger = LoggerFactory.getLogger(ContentIndexerScheduler::class.java)
     }
 
     @Scheduled(cron = "\${autoposting-bot.scheduler.content-indexer-cron}")
     fun indexContent() = runBlocking {
+        logger.info("Content indexing run")
+
         val lastScheduledAt = schedulerInfoRepository.findByName(SCHEDULER_NAME)!!.scheduledAt
         val contentMetadataList = diskClient.getImageContentByDate(lastScheduledAt)
             .map { ContentMetadata(it.name, it.path, it.createdAt) }
@@ -32,5 +36,7 @@ class ContentIndexerScheduler(val diskClient: YandexDiskClient,
         //contentMetadataRepository.saveAll(contentMetadataList)
 
         schedulerInfoRepository.save(SchedulerInfo(SCHEDULER_NAME, LocalDateTime.now()))
+
+        logger.info("Content indexing done, new content count: ${contentMetadataList.size}")
     }
 }
