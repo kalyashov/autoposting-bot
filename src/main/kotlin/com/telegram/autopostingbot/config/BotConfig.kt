370a0532ad8b534464b8f6b352c121f1576a428a
kalyashov <kalyashov@yandex.ru>
2021-10-15 20:02:35 +0300

    bot config update

--- src/main/kotlin/com/telegram/autopostingbot/config/BotConfig.kt
+++ src/main/kotlin/com/telegram/autopostingbot/config/BotConfig.kt
@@ -4,5 +4,15 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConstructorBinding
-@ConfigurationProperties(prefix = "bot")
-data class BotConfig(val name: String, val token: String)
+@ConfigurationProperties(prefix = "autoposting-bot")
+data class BotConfig(val bot: Bot, val disk: Disk, val watermark: Watermark) {
+    data class Bot(val name: String, val token: String, val channelId: String, val adminList: Set<String>)
+    data class Disk(val userAgent: String, val token: String, val folder: String, val endpoint: String)
+    data class Watermark(val imgSrc: String,
+                         val verticalTextSrc: String,
+                         val horizontalTextSrc: String,
+                         val imgWmMinHeight : Int,
+                         val imgOpacity: Float,
+                         val textOpacity: Float,
+                         val text: String)
+}
